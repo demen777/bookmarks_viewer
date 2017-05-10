be4129a9254325a48e7049cb1c60defb2e4a99c3
demen777 <demen7@gmail.com>
2017-05-10 17:16:34 +0500

    Add context menu (without working action yet)

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function showTree() {
 }
 
 function handleGetTree(bookmarkTreeNodes) {
-    var tree = createTree('div_tree', 'white');
+    var context_menu = generateContextMenu();
+    var tree = createTree('div_tree', 'white', context_menu);
     tree.doubleClickNode = openUrl;
     var curProcessChildNode = processChildNode.bind(null, tree);
     bookmarkTreeNodes[0].children.forEach(curProcessChildNode);
@@ -15,9 +16,10 @@ function handleGetTree(bookmarkTreeNodes) {
 function processChildNode(parent, node) {
     var treeNode = ('parent' in parent)
         // parent is node 
-        ? parent.createChildNode(node.title, false, choiceIcon(node), node.url, 'context1')
+        ? parent.createChildNode(node.title, false, choiceIcon(node), node.url, 
+            node.url ? 'bookmark_context_menu' : 'folder_context_menu')
         // parent is tree
-        : parent.createNode(node.title, false, choiceIcon(node), null, null, 'context1');
+        : parent.createNode(node.title, false, choiceIcon(node), null, null, 'first_level_folder_context_menu');
     var curProcessChildNode = processChildNode.bind(null, treeNode);
     if (node.children) {
         node.children.forEach(curProcessChildNode);
@@ -35,4 +37,45 @@ function openUrl(node) {
             url: node.tag
         });
     }
+}
+
+function generateContextMenu() {
+    var enableAutoExpandMenuItem = 
+    { 
+        text: 'Enable auto expand',
+        icon: 'images/expand.png',
+        action: function(node) {}
+    };
+    var disableAutoExpandMenuItem = 
+    { 
+        text: 'Disable auto expand',
+        icon: 'images/expand.png',
+        action: function(node) {}
+    };    
+    var enableAutoHideMenuItem = 
+    { 
+        text: 'Enable auto hide',
+        icon: 'images/expand.png',
+        action: function(node) {}
+    };
+    var res = 
+    {
+        'first_level_folder_context_menu': {
+            elements: [
+                enableAutoExpandMenuItem,
+                disableAutoExpandMenuItem,
+                enableAutoHideMenuItem
+            ]
+        },
+        'folder_context_menu': {
+            elements: [
+                enableAutoExpandMenuItem,
+                disableAutoExpandMenuItem
+            ]
+        },
+        'bookmark_context_menu': {
+            elements: []
+        }
+    };
+    return res;
 }
